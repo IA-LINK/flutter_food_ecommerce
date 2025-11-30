7889690d5df3dd28942ea15086c479f7c93543f9
IA-LINK <jyc.kom@gmail.com>
2025-11-30 19:24:15 +0100

    empt my code files

--- food_ecommerce/android/app/src/main/kotlin/com/codeagain/food_ecommerce/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.codeagain.food_ecommerce
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- food_ecommerce/lib/services/payment_service.dart
+++
@@ -1,6 +0,0 @@
-class PaymentService {
-  Future<void> processPayment(double amount) async {
-    // Integrate with Stripe or Flutterwave later
-    print("Processing payment of ₦$amount...");
-  }
-}
